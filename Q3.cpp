a4591df0276e9860b531ef0250ef81c121a98174
9381613309 <mems250005034@iiti.ac.in>
2025-10-17 17:09:10 +0530

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -10,24 +10,15 @@ int main() {
     Point points[7];
     int countFirstQuadrant = 0;
 
-    // Read 7 points from user input
-    cout << "Enter coordinates for 7 points (x y):
-";
+    cout << "Enter coordinates for 7 points (x y):";
     for (int i = 0; i < 7; i++) {
         cout << "Point " << i + 1 << ": ";
         cin >> points[i].x >> points[i].y;
     }
-
-    // Count points lying in the first quadrant
     for (int i = 0; i < 7; i++) {
         if (points[i].x > 0 && points[i].y > 0)
             countFirstQuadrant++;
     }
-
-    // Display result
-    cout << "
-Number of points in the first quadrant: " << countFirstQuadrant << endl;
-
+    cout << "Number of points in the first quadrant: " << countFirstQuadrant << endl;
     return 0;
 }
-
