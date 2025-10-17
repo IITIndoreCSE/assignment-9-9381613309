3a125c661c83157a3bdd150be1dd6860830f6b6c
9381613309 <mems250005034@iiti.ac.in>
2025-10-17 16:04:16 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,1 +1,28 @@
+#include <iostreme>
+using namespace std;
 
+struct Item {
+    int id;
+    float cost;
+};
+
+int main() {
+    // Initialize first four elements, rest default to zero
+    Item items[6] = {
+    {101, 45.75} 
+    {102, 60.00}
+    {103, 80.50}
+    {104, 30.25}
+    // Remaining two elements are default-intialized
+}
+
+count << "items with cost greater than 50.00:" << endl;
+for(int i = 0; i <6; ++i) {
+    if(items[i].cost > 50.00) {
+        cout << "ID: " << items[i].id
+             << ", Cost: " <<
+items[i].cost << endl;
+        }
+     }
+     return 0;
+}
