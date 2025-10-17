c81e2cc1e4f3afda55bc7b3f8860a0aca11c2da7
9381613309 <mems250005034@iiti.ac.in>
2025-10-17 16:21:23 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -7,22 +7,20 @@ struct Item {
 };
 
 int main() {
-    // Initialize first four elements, rest default to zero
     Item items[6] = {
-    {101, 45.75} 
-    {102, 60.00}
-    {103, 80.50}
+    {101, 45.75},
+    {102, 60.00},
+    {103, 80.50},
     {104, 30.25}
-    // Remaining two elements are default-intialized
-}
-
-count << "items with cost greater than 50.00:" << endl;
-for(int i = 0; i <6; ++i) {
-    if(items[i].cost > 50.00) {
-        cout << "ID: " << items[i].id
-             << ", Cost: " <<
-items[i].cost << endl;
-        }
-     }
-     return 0;
+    }
+    
+    cout << "Items with cost greater than 50.00:" << endl;
+    for(int i = 0; i <6; ++i) {
+        if(items[i].cost > 50.00) {
+            cout << "ID: " << items[i].id
+                 << ", Cost: " <<
+    items[i].cost << endl;
+            }
+         }
+         return 0;
 }
